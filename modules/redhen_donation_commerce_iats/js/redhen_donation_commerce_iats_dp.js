857778fd2b832991ca847b467ef3758a74640b0f
ruscoe <danruscoe@gmail.com>
2018-08-01 01:56:56 -0700

    Transfer donation form values to direct post form values on submit.

--- modules/redhen_donation_commerce_iats/js/redhen_donation_commerce_iats_dp.js
+++ modules/redhen_donation_commerce_iats/js/redhen_donation_commerce_iats_dp.js
@@ -7,49 +7,78 @@
 
     Drupal.behaviors.redhen_donation_commerce_iats_dp = {
         attach: function(context, settings) {
-
-            console.log('Attached JS: redhen_donation_commerce_iats_dp');
-
             // Attach click handler to populate direct post form fields on submit.
             $('#redhen-donation-form #edit-submit', context).unbind('click').bind('click', function() {
-
-                console.log('Click handler for redhen_donation_commerce_iats_dp');
-
                 // Populate direct post form fields.
 
+                // TODO: ?
                 // IATS_DPM_ProcessID
+
                 // IATS_DPM_Amount
+                // TODO: Needs to work for custom donation amounts.
+                var donation = $("select[name=donation]").val();
+                $("input[name=IATS_DPM_Amount]").val(donation);
+
+                // TODO: Invoice number.
                 // IATS_DPM_Invoice
+
+                // General information about the transaction. May not be needed.
                 // IATS_DPM_Comment
+
                 // IATS_DPM_FirstName
+                var first_name = $("input[name=first_name]").val();
+                $("input[name=IATS_DPM_FirstName]").val(first_name);
+
                 // IATS_DPM_LastName
+                var last_name = $("input[name=last_name]").val();
+                $("input[name=IATS_DPM_LastName]").val(last_name);
+
                 // IATS_DPM_Address
+                var address = $("input[name='commerce_customer_address[und][0][thoroughfare]']").val();
+                $("input[name=IATS_DPM_Address]").val(address);
+
                 // IATS_DPM_City
+                var city = $("input[name='commerce_customer_address[und][0][locality]']").val();
+                $("input[name=IATS_DPM_City]").val(city);
+
+                // TODO
                 // IATS_DPM_Province
+
                 // IATS_DPM_Country
+                var country = $("select[name='commerce_customer_address[und][0][country]']").val();
+                $("input[name=IATS_DPM_Country]").val(country);
+
+                // TODO
                 // IATS_DPM_ZipCode
 
                 // Method of payment.
                 // IATS_DPM_MOP
+                var mop = $("select[name='payment_details[credit_card][type]']").val();
+                $("input[name=IATS_DPM_MOP]").val(mop);
 
                 // Card number.
                 // IATS_DPM_AccountNumber
+                var number = $("input[name='payment_details[credit_card][number]']").val();
+                $("input[name=IATS_DPM_AccountNumber]").val(number);
 
                 // Card expiration date.
                 // IATS_DPM_ExpiryDate
 
+                var expiry_month = $("select[name='payment_details[credit_card][exp_month]']").val();
+                var expiry_year = $("select[name='payment_details[credit_card][exp_year]']").val();
+
+                var expiry_string = expiry_month + '/' + expiry_year[2] + expiry_year[3];
+
+                $("input[name=IATS_DPM_ExpiryDate]").val(expiry_string);
+
                 // Card security code.
                 // IATS_DPM_CVV2
+                var cvv2 = $("input[name='payment_details[credit_card][code]']").val();
+                $("input[name=IATS_DPM_CVV2]").val(cvv2);
 
-                // TODO: Parse card expiration date.
-                //var expiry_month = $("select[name=internal_iats_dpm_exp_month]").val();
-                //var expiry_year = $("select[name=internal_iats_dpm_exp_year]").val();
+                // TODO: Post payment information to iATS.
 
-                //var expiry_string = expiry_month + '/' + expiry_year[2] + expiry_year[3];
-
-                //$("input[name=IATS_DPM_ExpiryDate]").val(expiry_string);
-
-                // TODO: Disable submit button - prevent duplicate submissions?
+                // Disable submit button - prevent duplicate submissions?
                 return false;
             });
         }
