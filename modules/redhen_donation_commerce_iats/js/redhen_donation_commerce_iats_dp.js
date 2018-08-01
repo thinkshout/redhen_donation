fa42b7298649465ed2334a88a763b0ba48321148
ruscoe <danruscoe@gmail.com>
2018-08-01 00:36:18 -0700

    Attach direct post form fields / javascript to redhen donation form.

--- modules/redhen_donation_commerce_iats/js/redhen_donation_commerce_iats_dp.js
+++ modules/redhen_donation_commerce_iats/js/redhen_donation_commerce_iats_dp.js
@@ -7,11 +7,41 @@
 
     Drupal.behaviors.redhen_donation_commerce_iats_dp = {
         attach: function(context, settings) {
-            // Attach click handler to populate direct post form elements on submit.
-            $('.checkout-continue', context).unbind('click').bind('click', function() {
 
-                console.log('redhen_donation_commerce_iats_dp');
+            console.log('Attached JS: redhen_donation_commerce_iats_dp');
 
+            // Attach click handler to populate direct post form fields on submit.
+            $('#redhen-donation-form #edit-submit', context).unbind('click').bind('click', function() {
+
+                console.log('Adding click handler via redhen_donation_commerce_iats_dp');
+
+                // Populate direct post form fields.
+
+                // IATS_DPM_ProcessID
+                // IATS_DPM_Amount
+                // IATS_DPM_Invoice
+                // IATS_DPM_Comment
+                // IATS_DPM_FirstName
+                // IATS_DPM_LastName
+                // IATS_DPM_Address
+                // IATS_DPM_City
+                // IATS_DPM_Province
+                // IATS_DPM_Country
+                // IATS_DPM_ZipCode
+
+                // Method of payment.
+                // IATS_DPM_MOP
+
+                // Card number.
+                // IATS_DPM_AccountNumber
+
+                // Card expiration date.
+                // IATS_DPM_ExpiryDate
+
+                // Card security code.
+                // IATS_DPM_CVV2
+
+                // TODO: Parse card expiration date.
                 //var expiry_month = $("select[name=internal_iats_dpm_exp_month]").val();
                 //var expiry_year = $("select[name=internal_iats_dpm_exp_year]").val();
 
